1da76e04d2acb8264fe5a8b22b9ecb66a66c71da
Guillaume Guerdoux <guillaume.guerdoux>
2018-12-25 19:18:35 +0100

    send mail in view

--- django_production/user/views.py
+++ django_production/user/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from user.tasks import send_email_task
+from django.core.mail import mail_managers
 
 def send_email(request):
     print('EMAIL SENT')
-    send_email_task.delay()
+    # send_email_task.delay()
+    mail_managers(subject="Test from Django production", message="Message de test de django production avec Celery")
     return render(request, 'home.html')
