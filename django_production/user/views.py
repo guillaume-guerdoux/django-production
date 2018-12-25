a0d84cf92df9fed34c9617a14971ce896ea17115
Guillaume Guerdoux <guillaume.guerdoux>
2018-12-25 16:38:08 +0100

    remove celery for the moment

--- django_production/user/views.py
+++ django_production/user/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from user.tasks import send_email_task
 
 def send_email(request):
+    print('EMAIL SENT')
     send_email_task.delay()
     return render(request, 'home.html')
